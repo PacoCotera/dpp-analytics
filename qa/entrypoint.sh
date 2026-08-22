acf64f0f3ae4dc44edb20806557814dc9fe7b874
Paco Cotera <paco@cotera.org>
2026-08-22 02:35:58 -0600

    Run Geography zoom regression gate

--- qa/entrypoint.sh
+++ qa/entrypoint.sh
@@ -12,6 +12,8 @@ node /qa/numeric_ui_qa.mjs "$base_url" "$work_dir"
 numeric_rc=$?
 node /qa/geography_qa.mjs "$base_url" "$work_dir"
 geography_rc=$?
+node /qa/geography_zoom_qa.mjs "$base_url" "$work_dir"
+geography_zoom_rc=$?
 node /qa/footer_qa.mjs "$base_url" "$work_dir"
 footer_rc=$?
 cp -a "$work_dir"/. "$out_root"/ 2>/dev/null || true
@@ -19,4 +21,5 @@ if [ "$visual_rc" -ne 0 ]; then exit "$visual_rc"; fi
 if [ "$nav_rc" -ne 0 ]; then exit "$nav_rc"; fi
 if [ "$numeric_rc" -ne 0 ]; then exit "$numeric_rc"; fi
 if [ "$geography_rc" -ne 0 ]; then exit "$geography_rc"; fi
+if [ "$geography_zoom_rc" -ne 0 ]; then exit "$geography_zoom_rc"; fi
 exit "$footer_rc"
